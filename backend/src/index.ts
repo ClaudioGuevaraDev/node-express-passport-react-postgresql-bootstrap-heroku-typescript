821b3d202d27847de5445cbb6b07f7a3f9cee9e3
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-07-25 09:18:03 -0400

    Express configured

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello World")
+import app from "./app";
+
+(() => {
+  try {
+    app.listen(app.get("port"));
+    console.log("Server listening on port", app.get("port"));
+  } catch (error) {
+    console.error("Error to initialize the server.");
+  }
+})();
